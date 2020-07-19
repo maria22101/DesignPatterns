a75ddcd1e4b0aeda90ae0824da71569e10c5e29c
Mariia Bilous <maria.bilous101@gmail.com>
2020-07-19 23:03:21 +0300

    implement adapter example

---
+++ src/structural/adapter/duck_and_drone/RedHeadDuck.java
@@ -0,0 +1,13 @@
+package structural.adapter.duck_and_drone;
+
+public class RedHeadDuck implements Duck{
+    @Override
+    public void quack() {
+        System.out.println("Quack");
+    }
+
+    @Override
+    public void fly() {
+        System.out.println("I'm flying");
+    }
+}
