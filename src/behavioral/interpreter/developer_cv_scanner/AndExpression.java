d3543a9df1b3659b749892e0c2809cfb5b2c4a28
Mariia Bilous <mariia_bilous@epam.com>
2020-07-28 17:13:11 +0300

    implement interpreter example - developers cv scanner

---
+++ src/behavioral/interpreter/developer_cv_scanner/AndExpression.java
@@ -0,0 +1,17 @@
+package behavioral.interpreter.developer_cv_scanner;
+
+public class AndExpression implements Expression{
+    Expression expression1;
+    Expression expression2;
+
+    public AndExpression(Expression expression1, Expression expression2) {
+        this.expression1 = expression1;
+        this.expression2 = expression2;
+    }
+
+    @Override
+    public boolean interpret(String context) {
+        return expression1.interpret(context) &&
+                expression2.interpret(context);
+    }
+}
---
+++ src/behavioral/interpreter/developer_cv_scanner/Expression.java
@@ -0,0 +1,6 @@
+package behavioral.interpreter.developer_cv_scanner;
+
+public interface Expression {
+
+    public boolean interpret(String context);
+}
